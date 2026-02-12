ede9befc2221cd8951e2b623dc263bfac6073d4e
guyweir <guy.weir@wpieconomics.com>
2026-02-12 16:48:36 +0000

    typos

--- app.r
+++ app.r
@@ -254,9 +254,9 @@ ui <-
             card(
               card_header("Data Sources"),
               card_body(HTML("<h3>Kings Trust Sources:</h3>
-                             <p><b>Partipants</B> extracted 22-08-2025, covering 01-04-2019 to 31-03-2025</p>
+                             <p><b>Participants</B> extracted 22-08-2025, covering 01-04-2019 to 31-03-2025</p>
                              <p><b>Outcomes Survey</B> extracted 10-07-2025, covering 20-01-2022 to 16-05-2025</p>
-                             <p><b>HCS Outcomes</B> extracted 20-05-2025, covering 02-10-2023 to 19-05-2025</p>
+                             <p><b>HSC Outcomes</B> extracted 20-05-2025, covering 02-10-2023 to 19-05-2025</p>
                              <p><b>My Journey</B> extracted 20-05-2025, covering 01-04-2019 to 30-04-2025</p>
                              <p><b>Wellbeing Survey HSC data</B> extracted 24-06-2025, covering 01-04-2024 to 24-06-2025</p>
                              <p><b>The Prince's Trust Wellbeing Survey</B> extracted 27-05-2025, 01-03-2023</p>
